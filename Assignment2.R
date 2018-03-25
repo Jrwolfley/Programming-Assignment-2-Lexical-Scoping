9b2bd877d28b2ee13bea964e4632f666345d1f8a
Jrwolfley <37673860+Jrwolfley@users.noreply.github.com>
2018-03-25 17:06:03 -0400

    Add files via upload

---
+++ Assignment2.R
@@ -0,0 +1,30 @@
+## makeCacheMatrix creates a matrix object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inverse) inv <<- inverse
+        getInv <- function() inv
+        list(set = set,
+             get = get,
+             setInv = setInv,
+             getInv = getInv)
+}
+
+## CacheSolve computes the inverse of the matrix created by 'makeCacheMatrix'
+
+cacheSolve <- function(x, ...) {
+        inv <- x$getInv()
+        if (!is.null(inv)) {
+                message("getting cached")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInv(inv)
+        inv
+}
